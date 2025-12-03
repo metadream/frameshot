d3e2bd549ac502c7df123122867d6d8f7ffc15fe
metadream <mophisoft@gmail.com>
2025-12-03 10:49:36 +0800

    drag to resize sidebar

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,13 +1,34 @@
 import { $ } from "../main/utils.js";
 import thumbnail from "./thumbnail.js";
 
+const dragger = $(".dragger");
+const sidebar = $('aside');
+const openBtn = $('#open-btn');
+
 const picturePaths = await electron.getPicturePaths();
 thumbnail.render(picturePaths);
 
-$("#open-btn").onclick = async function() {
+openBtn.onclick = async function() {
     const { filePaths } = await electron.openFileDialog();
     if (filePaths && filePaths.length >= 1) {
         thumbnail.render(filePaths);
         electron.updateConfig("picture_paths", filePaths);
     }
 }
+
+dragger.onmousedown = function(e) {
+    const clientX = e.clientX;
+    const offsetLeft = dragger.offsetLeft;
+
+    document.onmousemove = function(e) {
+        const distance = offsetLeft + (e.clientX - clientX);
+        sidebar.style.width = distance + "px";
+        return false;
+    };
+
+    document.onmouseup = function() {
+        document.onmousemove = null;
+        document.onmouseup = null;
+    };
+    return false;
+};
