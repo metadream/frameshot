1581bca2a9dcfe1a99e4712e8e1d24afcb5c925c
metadream <mophisoft@gmail.com>
2025-12-05 14:17:43 +0800

    create thumbnails

--- src/main/utils.js
+++ src/main/utils.js
@@ -10,6 +10,6 @@ export function formatBytes(bytes) {
     if (!bytes || bytes < 1) return "0";
     const unit = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB"];
     const base = Math.min(unit.length - 1, Math.floor(Math.log(bytes) / Math.log(1024)));
-    const scale = Math.max(0, base - 2);
+    const scale = Math.max(0, base);
     return parseFloat((bytes / Math.pow(1024, base)).toFixed(scale)) + " " + unit[base];
 }
