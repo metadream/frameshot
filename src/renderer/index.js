6e21d3616f15f9b4aab08f2a19478639dd9f1afd
seatwork <mophisoft@gmail.com>
2026-05-09 09:47:45 +0800

    rounded corner

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,12 +1,9 @@
 import { openImage } from "./control.js";
 
 // 显示应用名称和版本
-document.querySelector("#file-count").innerText = `${await electron.getAppName()} v${await electron.getAppVersion()}`;
-
-// 非Mac上设置窗体圆角边框
-if (electron.platform !== "darwin") {
-    document.body.classList.add("rounded-border");
-}
+const appName = await electron.getAppName();
+const appVersion = await electron.getAppVersion();
+document.querySelector("#file-count").innerText = `${appName} v${appVersion}`;
 
 // 监听应用启动后再从文件打开事件
 electron.onFileOpened(async (event, fileToOpen) => {
